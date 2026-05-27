005c7a56f7807fd988285a1af3955b36754e7485
Omegabot-2022 <120841437+Omegabot-2022@users.noreply.github.com>
2026-05-26 18:14:35 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+TOKEN = os.environ.get("LICHESS_TOKEN")
+
+TOURNAMENTS = [
+    {
+        "name": "International Bullet Arena",
+        "clockTime": 1,
+        "clockIncrement": 0,
+        "minutes": 360,
+        "variant": "standard",
+        "schedule": "0 0 * * *",
+        "team": "the-international-bullet-league",
+        "daysInAdvance": 1,
+        "startTime": "06:00"
+    },
+    {
+        "name": "International Bullet Arena",
+        "clockTime": 1,
+        "clockIncrement": 0,
+        "minutes": 360,
+        "variant": "standard",
+        "schedule": "0 0 * * *",
+        "team": "the-international-bullet-league",
+        "daysInAdvance": 1,
+        "startTime": "12:00"
+    },
+    {
+        "name": "International Bullet Arena",
+        "clockTime": 1,
+        "clockIncrement": 0,
+        "minutes": 360,
+        "variant": "standard",
+        "schedule": "0 0 * * *",
+        "team": "the-international-bullet-league",
+        "daysInAdvance": 1,
+        "startTime": "18:00"
+    },
+    {
+        "name": "International Bullet Arena",
+        "clockTime": 1,
+        "clockIncrement": 0,
+        "minutes": 360,
+        "variant": "standard",
+        "schedule": "0 0 * * *",
+        "team": "the-international-bullet-league",
+        "daysInAdvance": 1,
+        "startTime": "24:00"
+    }
+]
